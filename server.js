92cd791431d3b218d5e4069d1fa30f47c4c43ec5
jjose96 <josephjose.967@gmail.com>
2021-03-19 08:26:17 +0530

    Consumer portal added

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     res.set('Cache-Control', 'no-store')
     next()
 })
-app.use(express.static(__dirname + 'dist/SmartE'));
+app.use(express.static(__dirname + 'dist/SmartE/'));
 app.get('/*', function(req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/SmartE/index.html'));

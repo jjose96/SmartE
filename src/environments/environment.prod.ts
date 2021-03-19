73225a955a4629d1afbe1ec25f8a125bec179cad
jjose96 <josephjose.967@gmail.com>
2021-03-19 19:09:29 +0530

    Consumer portal added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://smarte.herokuapp.com/'
+  url: 'https://smarte.herokuapp.com'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'https://smarte.herokuapp.com/'
+  url: 'https://smarte.herokuapp.com'
 
 };
 
